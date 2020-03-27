f16f1471b5b6f3cafe707b41a035d2c7c596a2a4
RocketFirm <rocketfirm@Tamerlan.Dlink>
2020-03-27 23:54:44 +0600

    port to github

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
+
+class App extends Component {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init('cb28dc37-7af7-4dce-806a-9a26dc602df1'); // set kOSSettingsKeyAutoPrompt to false prompting manually on iOS
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+    OneSignal.setLogLevel(6, 0);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+  render() {
+    return (
+      <View
+        style={{
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text>One Signal Push Notifications</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
